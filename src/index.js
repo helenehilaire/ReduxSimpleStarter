7efd2f54477f5935d49927c8a54bb2016f5d99f3
Helene Hilaire <hhilaire@Helenes-MacBook-Pro.local>
2018-08-24 12:10:40 -0400

    Debug from scratch

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// React is used to create and manage our components while ReactDOM is used to
+// interact with the DOM.
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Step 1:  Create a new component. This component should produce some HTML.
+const App = () => {
+    return <div>Hi!</div>;
+}
+
+// Step 2: Take this component's generated HTML and put it on the page in the DOM.
+ReactDOM.render(<App />, document.querySelector('.container'));
